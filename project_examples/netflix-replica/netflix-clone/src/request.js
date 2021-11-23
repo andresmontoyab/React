c555ee31c4bdaf96dcc93b870fef3208ddf7dfae
andresmontoyab <and.montoya18@gmail.com>
2021-11-23 06:55:23 -0500

    Getting Start Netflix Clone

---
+++ project_examples/netflix-replica/netflix-clone/src/request.js
@@ -0,0 +1,11 @@
+const API_KEY = "879a6b342421cab0d964a99d574d52d7"
+export default {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
